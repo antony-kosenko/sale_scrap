904ebd11ec6ec7ec56863b379a5cf8fa4b51b878
“Anton <kosenko.anton.work@gmail.com>
2024-12-02 18:54:38 +0200

    added basic spider for scrapping

---
+++ salescrap/spiders/rztk_spider.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+import scrapy
+from scrapy.http import Response
+
+
+class RztkSpider(scrapy.Spider):
+    name = "rozetka"
+    start_urls = ["https://rozetka.com.ua/ua/wishlist/aa723160c54053078/"]
+
+    def parse(self, response: Response, **kwargs: Any) -> Any:
+        wishlist_items = response.css("div.goods-tile__content")
+        for item in wishlist_items:
+            item_title = item.css("span.goods-tile__title")
+            prices = item.css("div.goods-tile__prices")
+            yield {
+                "brand": item_title.css("strong.ng-star-inserted::text").get(),
+                "title": item_title.css("::text").getall()[-1].strip(),
+                "price_old": prices.css("div.goods-tile__price--old::text").get("").replace(u'\xa0', u' ') or None,
+                "price_current": prices.css("span.goods-tile__price-value::text").get("").replace(u'\xa0', u' '),
+            }
